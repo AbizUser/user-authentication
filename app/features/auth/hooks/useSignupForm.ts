0aab603f8ebe8951b95f8f1e44debf87ecee4824
AbizUser <aman.biz.t23@gmail.com>
2024-11-11 07:36:52 +0900

    add signup logic

--- app/features/auth/hooks/useSignupForm.ts
+++ app/features/auth/hooks/useSignupForm.ts
@@ -1,20 +1,48 @@
 import { SubmitHandler, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useSignupFormSchema } from "@/app/features/auth/lib/FormSchema";
+import { signupFormSchema } from "@/app/features/auth/lib/FormSchema";
 import { z } from "zod";
+import { supabase } from "../lib/supabaseClient";
+import { useRouter } from "next/navigation";
 
 export const useSignupForm = () => {
-  const form = useForm<z.infer<typeof useSignupFormSchema>>({
-    resolver: zodResolver(useSignupFormSchema),
+  const router = useRouter();
+  const form = useForm<z.infer<typeof signupFormSchema>>({
+    resolver: zodResolver(signupFormSchema),
     defaultValues: {
       username: "",
       email: "",
       password: "",
     },
   });
-  const onSubmit: SubmitHandler<z.infer<typeof useSignupFormSchema>> = (data) => {
+  const onSubmit: SubmitHandler<z.infer<typeof signupFormSchema>> = async (
+    data 
+  ) => {
     const { username, email, password } = data;
-    console.log(username, email, password)
+    try {
+      const { data, error: signUpError} = await supabase.auth.signUp({
+        email,
+        password,
+      })
+
+      if (signUpError) {
+        console.log(signUpError);
+        throw signUpError;
+      }
+      const { error: userError} = await supabase
+      .from("User")
+      .insert([{ id: data.user?.id, username, email }]);
+      if (userError) {
+        console.log(userError.message);
+        throw userError
+      }
+
+      router.push("auth/login")
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(err.message);
+      }
+    }
   }
 
   return { form, onSubmit };
--- app/features/auth/lib/FormSchema.ts
+++ app/features/auth/lib/FormSchema.ts
@@ -7,7 +7,7 @@ export const signupFormSchema = z.object({
   email: z.string().email({ message: "メールアドレスが正しくありません。"}),
   password: z
   .string()
-  .min(2, { message: "ユーザ名は2文字以上で入力してください。"}).max(10, {message: "パスワードは10文字以内で入力してください。"}),
+  .min(6, { message: "ユーザ名は5文字以上で入力してください。"}).max(10, {message: "パスワードは10文字以内で入力してください。"}),
 });
 
 export const loginFormSchema = z.object({
